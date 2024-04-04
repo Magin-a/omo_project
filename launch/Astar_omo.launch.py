3d1a4ba051aa8741407cae51b30651be82f10ff2
Magin-a <80820166+Magin-a@users.noreply.github.com>
2024-04-04 14:01:20 +0900

    Update Astar_omo.launch.py

--- launch/Astar_omo.launch.py
+++ launch/Astar_omo.launch.py
@@ -14,26 +14,17 @@ from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
     omo_dir = get_package_share_directory('omo_r1mini_gazebo')
-    launch_dir = os.path.join(omo_dir, 'launch')
     urdf_dir = get_package_share_directory('omo_r1mini_description')
     my_custom_pkg = get_package_share_directory('omo_r1mini_custom')
-
-    #turtlebot3########################################################
-    turtlebot3_multi_robot = get_package_share_directory('turtlebot3_multi_robot')
-    TURTLEBOT3_MODEL = 'burger'
-
-    urdf = os.path.join(
-        turtlebot3_multi_robot, 'urdf', 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
-    )
-    ####################################################################
     
     #cartographer
     cartographer_pkg = os.path.join(get_package_share_directory('omo_r1mini_cartographer'), 'launch')
     
     #rviz2
-    rviz2_p = os.path.join(
-        my_custom_pkg,
-        'rviz', 'Astar_omo_cartographer.rviz')
+    #if you have your rviz file, replace it.
+    # rviz2_p = os.path.join(
+    #     my_custom_pkg,
+    #     'rviz', '**.rviz')
     
     #world_map
     world = os.path.join(
@@ -43,24 +34,17 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # Names and poses of the robots
-    robots = {'name': '', 'x_pose': '0', 'y_pose': '0', 'z_pose': 0.01}
-        
-    
-    ROBOT_MODEL = 'omo'
+    robot = {'name': '', 'x_pose': '0', 'y_pose': '0', 'z_pose': 0.01}
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     declare_use_sim_time = DeclareLaunchArgument(
         name='use_sim_time', default_value=use_sim_time, description='Use simulator time'
     )
-    
-    enable_drive = LaunchConfiguration('enable_drive', default='false')
-    declare_enable_drive = DeclareLaunchArgument(
-        name='enable_drive', default_value=enable_drive, description='Enable robot drive node'
-    )  
 
-    # urdf = os.path.join(
-    #     urdf_dir, 'urdf', 'omo_r1mini' + '.urdf'
-    # )
+    urdf = os.path.join(
+        urdf_dir, 'urdf', 'omo_r1mini' + '.urdf'
+    )
+    sdf = os.path.join(omo_dir,'models', 'omo_r1mini', 'model.sdf')
 
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -77,7 +61,6 @@ def generate_launch_description():
 
     # Declare the launch options
     ld.add_action(declare_use_sim_time)
-    ld.add_action(declare_enable_drive)
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
     
@@ -85,71 +68,68 @@ def generate_launch_description():
     remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
 
     last_action = None
-    # Spawn turtlebot3 instances in gazebo
-    for robot in robots:
 
-        namespace = [ '/' + robot['name'] ]
+    # Spawn robot instances in gazebo
+    namespace = [ '/' + robot['name'] ]
 
-        # Create state publisher node for that instance
-        robot_state_publisher = Node(
-            package='robot_state_publisher',
-            namespace=namespace,
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time,
-                            'publish_frequency': 10.0}],
-            remappings=remappings,
-            arguments=[urdf],
-        )
+    # Create state publisher node for that instance
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        namespace=namespace,
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time,
+                        'publish_frequency': 10.0}],
+        remappings=remappings,
+        arguments=[urdf],
+    )
 
-        # Create spawn call
-        spawn_robot = Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=[ #os.path.join(turtlebot3_multi_robot,'models', 'turtlebot3_' + TURTLEBOT3_MODEL, 'model.sdf')
-                        #os.path.join(omo_dir,'models', 'omo_r1mini', 'model.sdf')
-                '-file', os.path.join(turtlebot3_multi_robot,'models', 'turtlebot3_' + TURTLEBOT3_MODEL, 'model.sdf'),
-                '-entity', robot['name'],
-                '-robot_namespace', namespace,
-                '-x', robot['x_pose'], '-y', robot['y_pose'],
-                '-z', '0.01', '-Y', '0.0',
-                '-unpause',
-            ],
-            output='screen',
-        )
+    # Create spawn call
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-file', sdf,
+            '-entity', robot['name'],
+            '-robot_namespace', namespace,
+            '-x', robot['x_pose'], '-y', robot['y_pose'],
+            '-z', '0.01', '-Y', '0.0',
+            '-unpause',
+        ],
+        output='screen',
+    )
 
-        # # cartographer
-        # omo_cartographer = IncludeLaunchDescription(
-        #     PythonLaunchDescriptionSource([cartographer_pkg, '/cartographer.launch.py']),
-        # ) #여기 쉼표 붙이면 tuple 오류 뜸
+    # cartographer
+    # omo_cartographer = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([cartographer_pkg, '/cartographer.launch.py']),
+    # )
 
+    ros2_Astar = Node(
+        package="omo_r1mini_custom",
+        executable="ros2_Astar.py",
+        arguments=[],
+        output='screen',
+    )
 
-        ros2_Astar = Node(
-            package="omo_r1mini_custom",
-            executable="ros2_Astar.py",
-            arguments=[],
-            output='screen',
-        )
+    if last_action is None:
+        # Call add_action directly for the first robot to facilitate chain instantiation via RegisterEventHandler
+        ld.add_action(robot_state_publisher)
+        ld.add_action(spawn_robot)
+        ld.add_action(ros2_Astar)
+        # ld.add_action(omo_cartographer)
 
-        if last_action is None:
-            # Call add_action directly for the first robot to facilitate chain instantiation via RegisterEventHandler
-            ld.add_action(robot_state_publisher)
-            ld.add_action(spawn_robot)
-            ld.add_action(ros2_Astar)
-            # ld.add_action(omo_cartographer)
-
-        else:
-            # Use RegisterEventHandler to ensure next robot creation happens only after the previous one is completed.
-            # Simply calling ld.add_action for spawn_entity introduces issues due to parallel run.
-            spawn_turtlebot3_event = RegisterEventHandler(
-                event_handler=OnProcessExit(
-                    target_action=last_action,
-                    on_exit=[spawn_robot,
-                            robot_state_publisher,
-                            ],
-                )
+    else:
+        # Use RegisterEventHandler to ensure next robot creation happens only after the previous one is completed.
+        # Simply calling ld.add_action for spawn_entity introduces issues due to parallel run.
+        spawn_turtlebot3_event = RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=last_action,
+                on_exit=[spawn_robot,
+                        robot_state_publisher,
+                        ],
             )
+        )
 
-            ld.add_action(spawn_turtlebot3_event)
+        ld.add_action(spawn_turtlebot3_event)
 
     return ld
